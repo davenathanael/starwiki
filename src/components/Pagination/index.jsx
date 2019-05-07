cd810a00d1e828e4699a728c5c0ee5de27fbca75
davenathanael <davenathanaeldev@gmail.com>
2019-05-07 17:52:36 +0700

    Added pagination component

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Page = styled.button`
+    margin: 1em;
+    padding: 1em;
+    outline: none;
+    background: ${props => props.active ? 'lightgrey' : 'none'};
+    border: 1px solid black;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const Pagination = ({ pages, active }) => (
+    <Wrapper>
+        {pages.map(p => 
+            <Page onClick={() => p.action()} type='button' active={p.pageNumber === active}>{p.pageNumber}</Page>
+        )}
+    </Wrapper>
+);
+
+Pagination.propTypes = {
+    pages: PropTypes.arrayOf(PropTypes.shape({
+        pageNumber: PropTypes.number.isRequired,
+        action: PropTypes.func,
+    })).isRequired,
+    active: PropTypes.number.isRequired,
+};
+
+export default Pagination;
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import CharacterList from '../../components/CharacterList';
+import Pagination from '../../components/Pagination';
+const pages = [
+    {
+        pageNumber: 1,
+        action: () => console.log("paginated to page 1")
+    },
+    {
+        pageNumber: 2,
+        action: () => console.log("paginated to page 2")
+    },
+    {
+        pageNumber: 3,
+        action: () => console.log("paginated to page 3")
+    },
+    {
+        pageNumber: 4,
+        action: () => console.log("paginated to page 4")
+    },
+    {
+        pageNumber: 5,
+        action: () => console.log("paginated to page 5")
+    },
+];
 
 const Home = () => (
     <div>
         <h2>Homepage</h2>
         <CharacterList />
+        <Pagination pages={pages} active={1} />
     </div>
 );
 
